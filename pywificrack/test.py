219287c8a2d322dedaf42875f0e3a7a8360f41fe
duan <duan@localhost.localdomain>
2015-09-12 12:22:04 +0800

    correct some errors

--- pywificrack/test.py
+++ pywificrack/test.py
@@ -1,9 +1,16 @@
 from pywificrack.util import *
+from pywificrack.crack import *
 from unittest import TestCase
 import unittest
 class TestMain(TestCase):
     def testUtil(self):
-        print Util.getWifiName()
+        print Util.getWifiNames()
+        print Util.getWirelessCards()
+    
+    def testCrack(self):
+        cards = Util.getWirelessCards()
+        crack = Crack(cards[0])
+        crack.run()
 
 if __name__ == '__main__':
     unittest.main()
--- pywificrack/util.py
+++ pywificrack/util.py
@@ -3,12 +3,38 @@ import shlex
 
 class Util(object):
     @classmethod
-    def getWifiName(self):
-        cmd0 = 'iwlist wlp3s0mon scan'
-        cmd1 = 'grep ESSID'
-        cmd2 = 'awk -F\\\" \'{print $2}\''
-        p0 = subprocess.Popen(shlex.split(cmd0), stdout = subprocess.PIPE)
-        p1 = subprocess.Popen(shlex.split(cmd1), stdin = p0.stdout, stdout = subprocess.PIPE)
-        p2 = subprocess.Popen(shlex.split(cmd2), stdin = p1.stdout, stdout = subprocess.PIPE)
-        out,err = p2.communicate()
-        print out
+    def getWifiNames(cls):
+        cards = Util.getWirelessCards()
+        if len(cards) < 1:
+            return None
+        cmds = [ 'iwlist ' + cards[0] + ' scan', 'grep ESSID', 'awk -F\\\" \'{print $2}\'']
+        return Util.runPipedCmds(cmds) 
+    
+    @classmethod
+    def getWifiNamesByCard(cls, card):
+        cmds = [ 'iwlist ' + card + ' scan', 'grep ESSID', 'awk -F\\\" \'{print $2}\'']
+        return Util.runPipedCmds(cmds) 
+
+    @classmethod
+    def getWirelessCards(cls):
+        cmds = ['sudo airmon-ng', 'awk \'{print $2}\'']
+        ret = []
+        for x in Util.runPipedCmds(cmds).split('\n')[1:]:
+            if len(x) > 0:
+                ret.append(x)
+        return ret 
+       
+    
+    @classmethod
+    def runPipedCmds(cls,cmds):
+        if len(cmds) < 1:
+            return None
+        plist = []
+        cmd0 = cmds[0]
+        p0 = subprocess.Popen(shlex.split(cmd0),stdout = subprocess.PIPE)
+        plist.append(p0)
+        for cmd in cmds[1:]:
+            pn = subprocess.Popen(shlex.split(cmd),stdin=plist[-1].stdout, stdout = subprocess.PIPE)
+            plist.append(pn)
+        out,err = plist[-1].communicate()
+        return out
